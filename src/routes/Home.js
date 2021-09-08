e9a8fc0f74f8957ad5978ff8bc9ca88c5c34911e
kimmsoll <kimmsoll@naver.com>
2021-09-08 12:52:17 +0900

    Update Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,16 +4,6 @@ import axios from "axios";
 import Movie from "../components/Movie";
 import "./Home.css";
 
-/* function 컴포넌트는 return 함수를 가진다.
-function App{
-  return <h1> I like Potato </h1>;
-}
-*/
-
-/* class 컴포넌트는 return 함수를 가지지 않고, render 함수를 가진다.
-React 는 자동으로 클래스 컴포넌트의 render 함수를 실행한다!
-class 컴포넌트 안에서 state 오브젝트를 사용할 수 있다.
-*/
 class Home extends React.Component{
   state = {
     isLoading: true,
@@ -58,36 +48,7 @@ class Home extends React.Component{
       )}
     </section>
     );
-  };
-
-  /* 기초 알기
-  state = {
-    count: 0
-  };
-  add = () => {
-    console.log("add");
-    // state를 바꾸고 싶을 때,
-    this.state.count = 1 을 한다면, 바뀌지 않는다.
-    대신, setState 함수를 호출하면, React 는 언제 어떻게 바꿔야 할 지 알아듣고 행동함!
-    //
-    this.setState(current => ({count: current.count + 1}));
-  }
-  minus = () => {
-    console.log("minus");
-    // this.setState({count: this.state.count - 1}) 보다 나은 방법
-    this.setState(current => ({count: current.count - 1}));
-  }
-  render() {
-    return (
-      // div 로 감싸주지 않으면 에러가 남!
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>add</button>
-        <button onClick={this.minus}>minus</button>
-      </div>
-    );
-  }
-  */
+  };   
 }
 
 export default Home;
